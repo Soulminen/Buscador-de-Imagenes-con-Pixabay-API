a7d42ce8861ca42c8a0db39da39fdd16882d7fc5
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-19 22:13:55 +0200

    Mostrando la cantidad de paginas del paginador

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
 
 const registrosPorPagina = 40;
 let totalPaginas;
@@ -49,7 +50,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '30036224-dc2a880da2bbf017b8ce6603c';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -102,10 +103,31 @@ function mostrarImagenes(imagenes) {
         `    
     });
 
+    while(paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild);
+
+    }
+
     imprimirPaginador();
 
 }
 
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas);
+
+    while (true) {
+        const { value, done} = iterador.next();
+        if(done) return;
+
+
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+
+        paginacionDiv.appendChild(boton);
+
+    }
+
 }
